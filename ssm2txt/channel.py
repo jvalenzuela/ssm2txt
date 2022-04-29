3980b303b8399e4543830d9aeb9534b9e8cf0e1c
Jason Valenzuela <jvalenzuela1977@gmail.com>
2022-04-29 15:23:43 -0400

    Create channel module.

---
+++ ssm2txt/channel.py
@@ -0,0 +1,58 @@
+"""
+This module defines the object handling content for channel nodes in the
+project tree.
+"""
+
+
+from ssm2txt.base import Node
+
+
+class Channel(Node):
+    """Generates output for a channel node.
+
+    Channels do not have any content(tabs), but serve only as an organizational
+    unit.
+    """
+
+    parent_attr = 'componentopoid'
+    tabs = []
+
+    # Identifies which channels are present in each of the categories.
+    categories = {
+        'Unknown': (),
+        'B': ('ch1',),
+        '1': ('ch1',),
+        '2': ('ch1', 'chTest'),
+        '3': ('ch1', 'ch2'),
+        '4': ('ch1', 'ch2')
+    }
+
+    # Mapping of channel type to display name.
+    names = {
+        'ch1': 'Channel 1',
+        'ch2': 'Channel 2',
+        'chTest': 'Test Channel'
+    }
+
+    @property
+    def show(self):
+        """
+        All channel types are always present in the source XML, so this
+        method filters out those not relevant to the subsystem's category.
+        """
+        return self.ch_type in self.categories[self.parent.category]
+
+    @property
+    def name(self):
+        """Channel names are fixed based on the channel type."""
+        return self.names[self.ch_type]
+
+    @property
+    def acronym(self):
+        """Channels use different acronyms between the 1/2 and test channels."""
+        return 'TE' if 'Test' in self.ch_type else 'CH'
+
+    @property
+    def ch_type(self):
+        """The channel type raw attribute value."""
+        return self.element.attrib['channeltype']
